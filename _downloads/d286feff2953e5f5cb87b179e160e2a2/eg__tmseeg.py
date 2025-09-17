bf437e2bbe0eab1dd2e0f0ae965a81466e33e078
JohnGriffiths <JohnGriffiths@users.noreply.github.com>
2025-09-17 20:33:34 +0000

    deploy: ac616bacd5308b76a579630a54bced51f9dc1912

--- _downloads/d286feff2953e5f5cb87b179e160e2a2/eg__tmseeg.py
+++ _downloads/d286feff2953e5f5cb87b179e160e2a2/eg__tmseeg.py
@@ -25,7 +25,7 @@ from whobpyt.datatypes import Parameter as par, Timeseries
 from whobpyt.models.jansen_rit import JansenRitModel,JansenRitParams
 from whobpyt.run import ModelFitting
 from whobpyt.optimization.custom_cost_JR import CostsJR
-from whobpyt.datasets.fetchers import fetch_egtmseeg
+from whobpyt.datasets.fetchers import fetch_momi2023
 
 # Python Packages used for processing and displaying given analytical data (supported for .mat and Google Drive files)
 import numpy as np
@@ -43,7 +43,7 @@ import mne # Neuroimaging package
 
 # %%
 # Download and load example data
-data_dir = fetch_egtmseeg()
+data_dir = fetch_momi2023()
 
 # %%
 # Load EEG data 
@@ -220,104 +220,6 @@ plt.plot(F.trainingStats.fit_params['c3'], label = "c3")
 plt.plot(F.trainingStats.fit_params['c4'], label = "c4")
 plt.legend()
 plt.title("Select Variables Changing Over Training Epochs")
-
-# %%
-#### rest is extral analysis on the neural states on different networks
-
-### get labels for Yeo 200
-url = 'https://raw.githubusercontent.com/ThomasYeoLab/CBIG/master/stable_projects/brain_parcellation/Schaefer2018_LocalGlobal/Parcellations/MNI/Centroid_coordinates/Schaefer2018_200Parcels_7Networks_order_FSLMNI152_2mm.Centroid_RAS.csv'
-atlas = pd.read_csv(url)
-labels = atlas['ROI Name']
-
-# get 7 networks 
-nets = [label.split('_')[2] for label in labels]
-net_names = np.unique(np.array(nets))
-
-
-
-
-# %%
-#### plot E response on each networks 
-fig, ax = plt.subplots(2,4, figsize=(12,10), sharey= True)
-t = np.linspace(-0.1,0.3, 400)
-
-for i, net in enumerate(net_names):
-    mask = np.array(nets) == net
-    ax[i//4, i%4].plot(t, F.lastRec['E'].npTS()[mask,:].mean(0).T)
-    ax[i//4, i%4].set_title(net)
-plt.suptitle('Test: E')
-plt.show()
-
-
-# %%
-### plot I response at each networks
-fig, ax = plt.subplots(2,4, figsize=(12,10), sharey= True)
-t = np.linspace(-0.1,0.3, 400)
-
-for i, net in enumerate(net_names):
-    mask = np.array(nets) == net
-    ax[i//4, i%4].plot(t, F.lastRec['I'].npTS()[mask,:].mean(0).T)
-    ax[i//4, i%4].set_title(net)
-plt.suptitle('Test: I')
-plt.show()
-
-# %%
-### plot P response at each networks
-fig, ax = plt.subplots(2,4, figsize=(12,10), sharey= True)
-t = np.linspace(-0.1,0.3, 400)
-
-for i, net in enumerate(net_names):
-    mask = np.array(nets) == net
-    ax[i//4, i%4].plot(t, F.lastRec['P'].npTS()[mask,:].mean(0).T)
-    ax[i//4, i%4].set_title(net)
-plt.suptitle('Test: P')
-plt.show()
-
-
-# %%
-### model provides a current and voltage which can be used to calculate the phase (no need Hilbert Transform)
-### plot phase of E at each network
-j = complex(0,1)
-fig, ax = plt.subplots(2,4, figsize=(12,10), sharey= True)
-t = np.linspace(-0.1,0.3, 400)
-
-phase = np.angle(F.lastRec['E'].npTS()+j*F.lastRec['Ev'].npTS())
-for i, net in enumerate(net_names):
-    mask = np.array(nets) == net
-    ax[i//4, i%4].plot(t, phase[mask,:].mean(0).T)
-    ax[i//4, i%4].set_title(net)
-plt.suptitle('Test: phase E')
-plt.show()
-
-# %%
-### plot I phase at each network
-j = complex(0,1)
-fig, ax = plt.subplots(2,4, figsize=(12,10), sharey= True)
-t = np.linspace(-0.1,0.3, 400)
-
-phase = np.angle(F.lastRec['I'].npTS()+j*F.lastRec['Iv'].npTS())
-for i, net in enumerate(net_names):
-    mask = np.array(nets) == net
-    ax[i//4, i%4].plot(t, phase[mask,:].mean(0).T)
-    ax[i//4, i%4].set_title(net)
-plt.suptitle('Test: phase I')
-plt.show()
-
-# %%
-### plot P phase at each network
-
-j = complex(0,1)
-fig, ax = plt.subplots(2,4, figsize=(12,10), sharey= True)
-t = np.linspace(-0.1,0.3, 400)
-
-phase = np.angle(F.lastRec['P'].npTS()+j*F.lastRec['Pv'].npTS())
-for i, net in enumerate(net_names):
-    mask = np.array(nets) == net
-    ax[i//4, i%4].plot(t, phase[mask,:].mean(0).T)
-    ax[i//4, i%4].set_title(net)
-plt.suptitle('Test: phase P')
-plt.show()
-
 
 # %%
 # Results Description
